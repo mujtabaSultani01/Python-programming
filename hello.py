d0bc61a84809323e69752d97d0a07d1d6c55a7ef
Mujtaba Sultani <mujtaba.cs01@kpu.edu.af>
2026-02-28 15:26:43 +0430

    Some basic operations

--- hello.py
+++ hello.py
@@ -1,4 +1,20 @@
 # Welcome to Python programming.
 # Here you'll learn different programming concepts from basics to advance using Python programming language.
 
-print("Hello python programming!")
+# print("Hello python programming!")
+
+# Variables in Python.
+
+price = 20  # Variable names are start with small characters.
+rating = 4.5
+name = 'Ali'
+is_published = True
+
+print(price)
+print(rating)
+print(name)
+print(is_published)
+
+
+
+
